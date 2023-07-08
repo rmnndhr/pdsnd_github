a3972024581e6147528a3d831d60de0bc2e68361
rmnndhr <rumi.manandhar@gmail.com>
2023-07-08 00:17:07 -0500

    fix: Fix AttributeError with new pandas version
    Fix the following error on pandas.version >= '1.0.0':
    AttributeError: 'DatetimeProperties' object has no attribute 'weekday_name'
    
    According to the following post:
    https://github.com/facebook/prophet/issues/1304
    - either replace .weekday_name with .day_name()
    - or downgrade to pandas version < 1.0.0

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -90,7 +90,7 @@ def load_data(city, month, day):
 
     # extract month and day of week from Start Time to create new columns
     df['Month'] = df['Start Time'].dt.strftime('%B')
-    df['Day of Week'] = df['Start Time'].dt.weekday_name
+    df['Day of Week'] = df['Start Time'].dt.day_name()
 
     # filter by month if applicable
     if month != 'all':
